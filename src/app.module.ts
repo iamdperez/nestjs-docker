d7b7cbb703a21e599842756fe9237742cf5b0a39
Daniel Perez <danperez27@gmail.com>
2021-06-30 16:13:24 -0600

    chore: connect api to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService, User } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3309,
+      username: 'root',
+      password: 'password123',
+      database: 'laravel',
+      synchronize: false,
+      migrationsRun: false,
+      entities: [User],
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
